afa158b6643a8ed0306447881fe23c347d93cc59
zamurda <murtazadhan02@gmail.com>
2022-11-17 15:33:53 +0000

    added adc_res() to get 'EnergyFits' variable

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,30 +1,52 @@
 import numpy as np
 from uproot_io import Events
 from numpy import poly1d
+from scipy.interpolate import interp1d
 
-def path_ratio(event_obj: Events, deg: int):
-    if isinstance(event_obj, Events):
-        ratios = np.zeros_like(event_obj.reco_hits_w)
-        for particle_idx, particle_hits in np.ndenumerate(event_obj.reco_particle_hits_w):
-            fit_coeffs = np.polyfit(event_obj.reco_hits_x_w[particle_idx[0]], particle_hits, deg)[0]
-            
-            #apply func to yvals
-            func = poly1d(fit_coeffs)
-            yvals = np.zeros_like(event_obj.reco_hits_x_w[particle_idx])
-            for j in len(yvals):
-                yvals[j] = func(event_obj.reco_hits_x_w[particle_idx][j])
-            
-            #length of best fit
-            f_prime = np.gradient(yvals)
-            path_length = np.trapz(
-                (np.sqrt(1 + np.gradient(event_obj.reco_hits_w, event_obj.reco_hits_x_w)) ** 2),
-                event_obj.reco_hits_x_w
-                )
-            
-            true_length = (particle_hits[-1] - particle_hits[0]) / (event_obj.reco_hits_x_w[particle_idx][-1] - event_obj.reco_hits_x_w[particle_idx][-1])
-            
-            ratios[particle_idx] = abs(path_length / true_length)
+def conf(event_obj: Events, metric, cut, invalid_ids):
+    #right of cut is shw
+    actual = np.delete(event_obj.is_shower, invalid_ids)
+    predicted = np.zeros_like(metric)
+    predicted[np.where(metric > cut)] = 1
+    
+    ttat = 0
+    ttas = 0
+    tsat = 0
+    tsas = 0
+    for e in zip(actual, predicted):
+        if e[0] == 0 and e[1] == 0:
+            ttat += 1
+        elif e[0] == 0 and e[1] == 1:
+            ttas += 1
+        elif e[0] == 1 and e[1] == 0:
+            tsat += 1
+        else:
+            tsas += 1
+    trk_correctness = ttat/len(np.where(actual == 0)[0])
+    shw_correctness = tsas/len(np.where(actual == 1)[0])
+    return [ttat, ttas, tsat, tsas], trk_correctness, shw_correctness
+
+def adc_res(event_obj: Events, iter: list):
+    res_arr = np.array([])
+    invalid = []
+    for idx in iter:
+        adc = event_obj.reco_adcs_w[idx]
+        vtx = (event_obj.reco_particle_vtx_x[idx], event_obj.reco_particle_vtx_w[idx])
+        xvals = event_obj.reco_hits_x_w[idx]
+        yvals = event_obj.reco_hits_w[idx]        
+        dist = np.array([])
+        for point in zip(xvals, yvals):
+            r = np.sqrt((point[0] - vtx[0])**2 + (point[1] - vtx[1])**2)
+            dist = np.append(dist, r)
         
-        return ratios
-    else:
-        raise TypeError(f"expected type uproot_io.Events, recieved {type(event_obj)}")
+        
+        if len(dist) != 0:
+            fit = np.polyfit(dist, adc, 1, full=True)
+            rms = np.sqrt(fit[1]/len(adc))
+            res_arr = np.append(res_arr, rms)
+        
+        else:
+            invalid.append(idx)
+            continue
+    
+    return res_arr, invalid
