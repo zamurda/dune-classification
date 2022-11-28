6d5bc0ec7dc9096517ff6a8e2468fc955865c067
zamurda <murtazadhan02@gmail.com>
2022-11-28 06:56:15 +0000

    update and fix Classifiers.py
    - added optional seeding for train-test split.
    - fixed issues with edge cases in compute_likelihoods()
    - added functionality to optimise divide

--- Classifiers.py
+++ Classifiers.py
@@ -29,7 +29,7 @@ class ProjectiveLikelihood:
     Implements Classifier interface.
     Creates a projective likelihood classifier which separates between trks (sig) and shws (bkg)
     """
-    def __init__(self, features: np.ndarray, target: list, tt_split=0.75, seeded=False):
+    def __init__(self, features: np.ndarray, target: list, tt_split=0.75, rowvars = True, seed=False):
         #psuedocount
         self.pc = 1.0
         if isinstance(features, np.ndarray):
@@ -38,13 +38,20 @@ class ProjectiveLikelihood:
             
             #train-test split the data
             vectors = features.transpose()
-            self.X_train, self.y_train, self.X_test, self.y_test = train_test_split(vectors,
-                                                                                    target,
-                                                                                    test_size=(1-tt_split))
+            if not seed:
+                self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(vectors,
+                                                                                        target,
+                                                                                        test_size=(1-tt_split))
+            else:
+                self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(vectors,
+                                                                                        target,
+                                                                                        test_size=(1-tt_split),
+                                                                                        random_state=int(seed))
             
             #prior probabilities
-            self.pr_sig = len(np.where(target==1)[0])/len(self.X_train)
-            self.pr_bkg = len(np.where(target==0)[0])/len(self.X_train)
+            n_particles = len(features[0])
+            self.pr_sig = len(np.where(target==1)[0])/n_particles
+            self.pr_bkg = len(np.where(target==0)[0])/n_particles
             
         else:
             raise TypeError(f"Expected features in np.ndarray, received {type(features)}")
@@ -56,17 +63,17 @@ class ProjectiveLikelihood:
         bkg_pdfs = np.array([])
         bkg_bin_edges = np.array([])
         for metric in self.X_train.transpose():
-            s_hist, s_widths = np.histogram(metric[np.where(self.y_train == 1)], bins=n_bins, density=True)
-            b_hist, b_widths = np.histogram(metric[np.where(self.y_train == 0)], bins=n_bins, density=True)
+            s_hist, s_edges = np.histogram(metric[np.where(self.y_train == 1)], bins=n_bins, density=True)
+            b_hist, b_edges = np.histogram(metric[np.where(self.y_train == 0)], bins=n_bins, density=True)
             sig_pdfs = np.append(sig_pdfs, s_hist)
-            sig_bin_edges = np.append(sig_bin_edges, s_widths)
+            sig_bin_edges = np.append(sig_bin_edges, s_edges)
             bkg_pdfs = np.append(bkg_pdfs, b_hist)
-            bkg_bin_edges = np.append(bkg_bin_edges, b_widths)
+            bkg_bin_edges = np.append(bkg_bin_edges, b_edges)
             
-        self.sig_pdfs = sig_pdfs.reshape(np.shape(self.features))
-        self.sig_bin_edges = sig_bin_edges.reshape(np.shape(self.features)[0], n_bins)
-        self.bkg_pdfs = sig_pdfs.reshape(np.shape(self.features))
-        self.bkg_bin_edges = sig_bin_edges.reshape(np.shape(self.features)[0], n_bins)
+        self.sig_pdfs = sig_pdfs.reshape(np.shape(self.X_train)[1], n_bins)
+        self.sig_bin_edges = sig_bin_edges.reshape(np.shape(self.X_train)[1], n_bins+1)
+        self.bkg_pdfs = bkg_pdfs.reshape(np.shape(self.X_train)[1], n_bins)
+        self.bkg_bin_edges = bkg_bin_edges.reshape(np.shape(self.X_train)[1], n_bins+1)
         
         #add psuedocount?
         
@@ -75,7 +82,7 @@ class ProjectiveLikelihood:
             return np.cov(self.X_train, rowvar=False)
             
 
-    def test(self, full=False, plot=True):
+    def test(self, plot=True, optimize=True):
         """
         --  full returns confusion matrix
         --  plot=True plots the likelihood ratio histogram
@@ -85,18 +92,39 @@ class ProjectiveLikelihood:
         #likelihood ratio
         l_s = compute_likelihoods(self.sig_pdfs, self.sig_bin_edges, self.X_test.transpose())
         l_b = compute_likelihoods(self.bkg_pdfs, self.bkg_bin_edges, self.X_test.transpose())
-        self.L_sig = (self.pr_sig*l_s)/(self.pr_sig*l_s+self.pr_bkg*l_b)
+        self.L_sig = (self.pr_sig*l_s)/((self.pr_sig*l_s)+(self.pr_bkg*l_b))
+        #self.L_sig = (l_s)/((l_s)+(l_b))
         
         #plot likelihood distribution
         if plot:
-            plt.hist(self.L_sig)
+            sig = self.L_sig[np.where(self.y_test==1)]
+            bkg = self.L_sig[np.where(self.y_test==0)]
+            plt.hist(sig, color="b")
+            plt.hist(bkg, color="r")
             plt.show()
         
-        #test model- L_sig>cut -> sig
-        pred = np.ones_like(self.L_sig)
-        pred[np.where(self.L_sig>self.cut)] = 1
-        if full:
-            return conf(pred, self.y_test)
+        #optimize cut if needed
+        if optimize:
+            
+            c = np.linspace(0,1,1001, endpoint=False)
+            tr = np.array([])
+            for cut in c:
+                pred = np.ones_like(self.L_sig)
+                pred[np.where(self.L_sig < cut)] = 0
+                a = conf(pred, self.y_test)
+                t = np.trace(a)
+                tr = np.append(tr, t/2)
+            self.cut = c[tr.argmax()]
+            pred = np.ones_like(self.L_sig)
+            pred[np.where(self.L_sig < self.cut)] = 0
+        
+        else:
+            self.cut = 0.5
+            pred = np.ones_like(self.L_sig)
+            pred[np.where(self.L_sig < self.cut)] = 0
+            
+        return conf(pred, self.y_test, diagnostic=False)
+        
     
     def predict(self, features: np.ndarray, rowvar=True):
         """
@@ -109,25 +137,45 @@ class ProjectiveLikelihood:
         
         l_s = compute_likelihoods(self.sig_pdfs, self.sig_bin_edges, features)
         l_b = compute_likelihoods(self.bkg_pdfs, self.bkg_bin_edges, features)
-        L_sig = (self.pr_sig*l_s)/(self.pr_sig*l_s+self.pr_bkg*l_b)
+        L_sig = (self.pr_sig*l_s)/((self.pr_sig*l_s)+(self.pr_bkg*l_b))
         
         p = np.ones_like(L_sig)
-        p[np.where(L_sig>self.cut)] = 1
+        p[np.where(L_sig < self.cut)] = 0
         
         return p
 
     def plot_roc(self):
-        pass
+        cuts = np.linspace(0,1,1001, endpoint=False)
+        self.efficiency = np.array([])
+        self.purity = np.array([])
+        for curr_cut in cuts:
+            temp_pred = np.zeros_like(self.L_sig)
+            temp_pred[np.where(self.L_sig>=curr_cut)] = 1
+            eff, pur = conf(temp_pred, self.y_test, diagnostic=True)
+                
+            self.efficiency = np.append(self.efficiency, eff)
+            self.purity = np.append(self.purity, pur)
+        plt.xlabel("Purity")
+        plt.ylabel("Efficiency")
+        plt.title(f"The optimal cut is at {np.around(self.cut, 2)}")
+        plt.plot(self.efficiency, self.purity)
+        plt.axvline(self.efficiency[np.where(cuts==self.cut)], ls="dotted", c="g")
+        plt.axhline(self.purity[np.where(cuts==self.cut)], ls="dotted", c="g")
+        plt.show()
 
 ########################################################################################################
 def compute_likelihoods(pdfs, bins, features):
     '''
     find multinomial likelihood array given an array of pdfs and array of features.
     '''
-    l = np.ones_like(pdfs[0])
+    l = np.ones_like(features[0])
     i=0
-    for pdf,feature in zip(pdf,features):
-        bin_num = np.digitize(feature, bins[i])
-        l = l * pdf[bins[i][bin_num]]
+    for pdf,feature in zip(pdfs,features):
+        pdf[np.where(pdf==0.0)] = np.min(pdf)/1000
+        bin_num = np.digitize(feature, bins[i]) - 1 #to get correct hist array index
+        #edge case handling
+        bin_num[np.where(bin_num == (len(bins[i])-1))] = len(bins[i])-2
+        bin_num[np.where(bin_num == -1)] = 0
+        l = l * pdf[bin_num]
         i += 1
     return l
