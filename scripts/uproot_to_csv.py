0fb2884452baa458af08ae7948266659e279d217
zamurda <murtazadhan02@gmail.com>
2022-12-26 00:13:16 +0000

    updated to extract from all reco files

--- scripts/uproot_to_csv.py
+++ scripts/uproot_to_csv.py
@@ -1,7 +1,13 @@
+import os
 import numpy as np
 import variables
-from csvtools.csvwrite import extract_from_uproot, array_to_csv, merge_csvs
 from uproot_io import Events
+from csvtools.csvwrite import (
+    extract_from_uproot,
+    array_to_csv,
+    merge_csvs
+)
+
 
 """
 Script for extracting features from a .root file through uproot_io.py
@@ -10,25 +16,38 @@ Creates a csv file in the current working directory with a specified name.
 
 fname = "testfeatures.csv"
 functions = [getattr(variables, func) for func in variables.__all__]
+default_headers = ["n_hits_total", "purity", "completeness", "pdg"]
 headers = []
 for funcname in variables.__all__:
     for direction in ["u", "v", "w"]:
         headers.append(f"{funcname}_{direction}")
 
+curr_path = os.path.dirname(__file__)
+rawdata_path = os.path.relpath(r"..\\rawdata")
 
-event = Events("c:/users/murta/documents/project_22/datasets/PandoraRecoFile_1.root")
-print(f"starting the extraction........")
-
-default_headers = ["n_hits_total", "purity", "completeness", "pdg"]
-default_data = np.array([
-                        event.reco_num_hits_total[:500],
-                        event.purity[:500],
-                        event.completeness[:500],
-                        event.mc_pdg[:500]
+def extract():
+    
+    for i in range(1,11):
+        fname = f"PandoraRecoFile_{i}.root"
+        product_name = f"PandoraFeatures_{i}.csv"
+        event = Events(f"{rawdata_path}\\{fname}")
+        
+        default_data = np.array([
+                        event.reco_num_hits_total,
+                        event.purity,
+                        event.completeness,
+                        event.mc_pdg
                         ]).transpose()
+        
+        array_to_csv(product_name, default_data, default_headers)
+        features = extract_from_uproot(event, functions, limit=None)
+        array_to_csv(product_name, features, headers)
+        array_to_csv(product_name, event.is_track, "target", rowvars=False)
+        
+filenames = [f"PandoraFeatures_{i}.csv" for i in range(1,11)]
+full_headers = default_headers + headers + ["target"]
 
-array_to_csv(fname, default_data, default_headers)
-
-features = extract_from_uproot(event, functions, limit=500)
-array_to_csv(fname, features, headers)
-array_to_csv(fname, event.is_track[:500], "target", rowvars=False)
+if __name__ == "__main__":
+    
+    extract()
+    merge_csvs(filenames, full_headers, "FullPandoraFeatures.csv")
